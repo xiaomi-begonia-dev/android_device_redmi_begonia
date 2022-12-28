80e1cddb198940e757ec65921d610cefce3f4115
Matsvei Niaverau <matvej2002@gmail.com>
2022-12-28 22:20:12 +0100

    begonia: configs: Kang powerhint from rosemary
    * Imported from mt6768-common tree & adapted for rosemary
    
    Change-Id: I0d5a58bebdb85cc6c2645084b7472f50dc20f8e7
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- device.mk
+++ device.mk
@@ -238,6 +238,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
