a870eb209c59d2e761630d9be8889a091f98f6b1
bengris32 <bengris32@protonmail.ch>
2022-12-29 17:08:44 +0000

    begonia: Switch to source built Power AIDL
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I90e336edd265e944fcbd2a889228c71605e77208
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- device.mk
+++ device.mk
@@ -300,6 +300,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
     android.hardware.power-V1-ndk.vendor
 
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-V1-mediatek
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/power_app_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_app_cfg.xml \
     $(LOCAL_PATH)/configs/power/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
