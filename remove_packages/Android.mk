72f0a63b20557899cfb46c4d3ba8ebe2681ace7a
MrErenK <akbaseren4751@gmail.com>
2023-01-08 13:41:35 +0300

    begonia: remove_packages: Re-organize debloat list
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -3,9 +3,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES += DiagnosticsToolPrebuilt
-LOCAL_OVERRIDES_PACKAGES += MaestroPrebuilt Maps ScribePrebuilt
-LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += DiagnosticsToolPrebuilt GoogleTTS
+LOCAL_OVERRIDES_PACKAGES += MaestroPrebuilt ScribePrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
