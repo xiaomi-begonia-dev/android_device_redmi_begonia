373777ed121074b2bb6c1417978a3d14d24ef99e
MrErenK <akbaseren4751@gmail.com>
2023-01-08 12:45:48 +0300

    begonia: Drop ANXCamera and build Aperture if exists
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- aosp_begonia.mk
+++ aosp_begonia.mk
@@ -21,7 +21,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_DOESNT_LIKE_FLIPENDO := true
 VOID_MAINTAINER := "EreN"
 TARGET_BOOT_ANIMATION_RES := 1080
-#TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_BUILD_APERTURE_CAMERA := true
 TARGET_ENABLE_BLUR := true
 WITH_GMS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -30,7 +30,6 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-$(call inherit-product, packages/apps/ANXCamera/config.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
