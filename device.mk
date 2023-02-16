e575712875e20f3d6c938b8c39114a5e74cb1f43
MrErenK <akbaseren4751@gmail.com>
2023-02-16 11:55:33 +0300

    begonia: Fix a typo
    xd
    
    Co-authored-by: Bishwo <bishwobhusal75@gmail.com>
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- device.mk
+++ device.mk
@@ -88,7 +88,7 @@ PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
 # Dex pre-opt for speed
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUIGoogle \
-    SettingsInlelligence \
+    SettingsIntelligence \
     SettingsProvider \
     SettingsGoogle \
     Launcher3QuickStep \
