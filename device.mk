87fd4a14133df1f34aec8fb1bc7b470d36b71353
MrErenK <akbaseren4751@gmail.com>
2023-03-04 17:02:29 +0300

    begonia: Remove overlay dir in device.mk * I have moved all overlays into RRO so no need those
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- device.mk
+++ device.mk
@@ -254,11 +254,6 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Permissions
