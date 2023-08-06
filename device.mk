872b570e2eec6affe88b9c64a3205e321321c79b
TTTT555 <albukerko@gmail.com>
2023-08-06 15:59:42 +0000

    begonia: Build `libnl` package
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Id285f7018eb1a53227dfe39fbf62bf6a5ba4a40f
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- device.mk
+++ device.mk
@@ -422,6 +422,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
+    libnl \
     android.hardware.wifi@1.0.vendor \
     android.hardware.wifi@1.1.vendor \
     android.hardware.wifi@1.2.vendor \
