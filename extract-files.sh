449ca59ce8674ea5e71f967da1151c0604c657ff
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2024-06-30 01:22:48 +0530

    begonia: patch libMtkOmxVdecEx.so
    also update sha1sum
    *needed after A14 QPR3
    
    E osal_utils: dlopen failed, dlopen failed: cannot locate symbol _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_ referenced by /vendor/lib/libMtkOmxVdecEx.so
    
    Signed-off-by: HyperNotAryanX97 <ahirearyan2016@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -108,6 +108,9 @@ function blob_fixup {
         vendor/lib64/libwifi-hal-mtk.so)
             "${PATCHELF}" --set-soname "libwifi-hal-mtk.so" "${2}"
             ;;
+        vendor/lib/libMtkOmxVdecEx.so)
+            grep -q "libui_shim.so" "$2" || "$PATCHELF" --add-needed "libui_shim.so" "$2"
+            ;;
     esac
 }
 
